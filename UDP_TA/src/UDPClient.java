e0f62f0080922584c62ffb22469f9b140f1d5fc8
unknown <s.rodriguezb1@uniandes.edu.co>
2019-10-20 21:02:04 -0500

    Ya funciona, lo basico

--- UDP_TA/src/UDPClient.java
+++ UDP_TA/src/UDPClient.java
@@ -1,23 +1,27 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 public class UDPClient 
 {
 	public static void main(String[] args) throws Exception
 	{
-		BufferedReader inFromUser = new BufferedReader (new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
 		
 		byte[] sendData = new byte[1024];
 		byte[] receiveData = new byte[1024];
 		
-		System.out.println("Solicite la conexión escribiendo 'Conexión'...");
+		PrintWriter bf = new PrintWriter(new BufferedWriter(new FileWriter("./data/logsCliente.txt",true)));
+		bf.write(new Date() + " \n");
+		bf.flush();
+		
+		bf.write("Solicite la conexión escribiendo 'Conexión'..." + "\n");
 		
 		String envia = "Conexión";
 		sendData = envia.getBytes();
@@ -32,7 +36,7 @@ public class UDPClient
 		
 		if(recibido.trim().equals("OK"))
 		{
-			System.out.println("Recibí confirmación del servidor");
+			bf.write("Recibí confirmación del servidor" + "\n");
 		}
 		
 		byte[] sendData2 = new byte[1024];
@@ -42,35 +46,107 @@ public class UDPClient
 		
 		DatagramPacket paqueteEnviadoArchivos = new DatagramPacket(sendData2, sendData2.length, IPAddress, 9876);
 		clientSocket.send(paqueteEnviadoArchivos);
+
+		bf.write("He enviado notificación de que puedo recibir archivos" + "\n");
 		
-		System.out.println("He enviado notificación de que puedo recibir archivos");
+		byte[] numP = new byte[1024];
 		
-		//De aquí para abajo no sé que onda, probablemente nada este bien
-		while(true) 
+		DatagramPacket paqueteNum = new DatagramPacket(numP, numP.length);
+		
+		clientSocket.receive(paqueteNum);
+		
+		int numPaquetes = Integer.parseInt(new String(paqueteNum.getData()).trim());
+		
+		int i = 0;
+		
+		System.out.println(numPaquetes);
+		
+		byte[] receiveData2 = new byte[8192];
+		
+		DatagramPacket paqueteArchivos = new DatagramPacket(receiveData2, receiveData2.length);
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		
+		byte[] rec = null;
+		
+		long tiempoInicial = System.nanoTime();
+		
+		do 
 		{	
-			
-			byte[] sendData3 = new byte[8192];
-			
-			DatagramPacket paqueteArchivos = new DatagramPacket(sendData3, sendData3.length, IPAddress, 9876);
+			clientSocket.setSoTimeout(50);
+			try {
 			clientSocket.receive(paqueteArchivos);
-			
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			}
+			catch(Exception e) {
+				break;
+			}
+			rec = paqueteArchivos.getData();
 	        
-			int read;
-			
-	        while((read = sendData3.length) != -1)
-	        {
-	            bos.write(sendData3, 0, read);
-	            
-	            bos.flush();
-	            
-	            System.out.println("RECIBÍ: " + sendData3);
-	            
+			DatagramPacket paqueteDigest = new DatagramPacket(receiveData, receiveData.length);
+	        try {
+			clientSocket.receive(paqueteDigest);
 	        }
+	        catch(Exception e){
+	        	break;
+	        }
+			String res = resultadoDigest(rec);
+			
+			String comp = new String(paqueteDigest.getData(),"UTF-8");
+			
+			comp = comp.trim();
+			
+			if(comp.equals(res)) {
+				bf.write("El paquete numero " + i +" llego correctamente" +  "\n");
+			}
+			else if(comp.length() - res.length() >= 1 && comp.substring(0, res.length()).equals(res))
+			{
+				bf.write("El paquete numero " + i +" llego correctamente" +  "\n");
+			}
+			else {
+				bf.write("El paquete numero " + i +" no llego correctamente" +  "\n");
+			}
+			bos.write(rec, 0, rec.length);
+			
+			
+	        bos.flush();
+	            
+	        i ++;
 	        
-	        
-	        		
 		}
+		while(i < numPaquetes);
+		
+		System.out.println("Sali");
+		
+		bf.write("El tiempo de transferencia de archivos fue de : " +(System.nanoTime() -tiempoInicial)/(1000000) +" segundos"+ "\n");
+		
+		if(i < numPaquetes) {
+			bf.write("El numero de paquetes perdidos fue de " + (numPaquetes - i) + "\n");
+		}
+		else
+		{
+			bf.write("No se perdieron paquetes, llegaron " + numPaquetes +" de " + numPaquetes + "\n");
+		}
+		
+		bf.close();
+		
+		clientSocket.close();
+	}
+	
+	public static String resultadoDigest(byte[] paquete) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		
+		md.update(paquete);
+		byte[] digest = md.digest();      
+
+		StringBuffer hexString = new StringBuffer();
+
+		for (int i = 0;i<digest.length;i++) 
+		{
+			hexString.append(Integer.toHexString(0xFF & digest[i]));
+		}
+		String prueba = hexString.toString();
+		
+		return prueba;
 	}
 }
 
--- UDP_TA/src/UDPServer.java
+++ UDP_TA/src/UDPServer.java
@@ -1,26 +1,45 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.Scanner;
 import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.net.*;
 
 public class UDPServer 
 {
 	public static void main(String[] args) throws Exception 
 	{
+		Scanner scanner = new Scanner(System.in);
 		DatagramSocket serverSocket = new DatagramSocket(9876);
+		System.out.println("Elija un archivo");
+		System.out.println("1-100MiB");
+		System.out.println("2-250MiB");
+
+		int arch = Integer.parseInt(scanner.nextLine());
 		
 		byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
 		
 		while(true)
 		{
+			PrintWriter bf = new PrintWriter(new BufferedWriter(new FileWriter("./data/logs.txt",true)));
+			bf.write(new Date() + " \n");
+			bf.flush();
+			
+			
 			DatagramPacket paqueteRecibido = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(paqueteRecibido);
 			
-			String recibido =  new String(paqueteRecibido.getData());
+			paqueteRecibido.getData();
 						
 			System.out.println("Me han solicitado conexión, enviando confirmación...");
 
@@ -46,26 +65,79 @@ public class UDPServer
 				System.out.println("Me han notificado que estan listos para recibir archivos");
 				System.out.println("Procedo a enviar un archivo");
 				
-				byte[] sendData3 = new byte[1024];
-				File archivo = new File("Foto.jpg");
+				File archivo = null;
+				
+				if(arch == 1) {
+					archivo = new File("./data/small.png");
+				}
+				else {
+
+					archivo = new File("./data/large.png");
+				}
+
+				bf.write("Se va a enviar el archivo "+ archivo.getPath() +" de tamano " + archivo.length() + " \n");
+
 				
 				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-			
+				
 				BufferedInputStream bis = new BufferedInputStream(new FileInputStream(archivo));
-					
+				
+				int numero = (int) (archivo.length()/8192)+1;
+				
+				String num = "" + numero;
+				long tiempoInicial = 0;
+				DatagramPacket paqueteNum = new DatagramPacket(num.getBytes(),num.getBytes().length,IPAddress,puerto);
+				serverSocket.send(paqueteNum);
+				System.out.println(num);
 				int in = 0;
 				byte[] byteArray = new byte[8192];
+				int cont = 0;
+				tiempoInicial = System.nanoTime();
 				while ((in = bis.read(byteArray)) != -1)
 				{
-					DatagramPacket paqueteArchivo = new DatagramPacket(byteArray, byteArray.length, IPAddress, 9876);
+					DatagramPacket paqueteArchivo = new DatagramPacket(byteArray, byteArray.length, IPAddress, puerto);
 					byteArrayOutputStream.write(byteArray,0,in);
+					serverSocket.send(paqueteArchivo);
+					String res = resultadoDigest(byteArray);
+					sendData = res.getBytes("UTF-8");
+					DatagramPacket paqueteDigest = new DatagramPacket(sendData, sendData.length, IPAddress, puerto);
+					serverSocket.send(paqueteDigest);
+					System.out.println("Holi " + in);
+					System.out.println("El digest es: " + res);
+					cont ++;
+					System.out.println(cont);
 				}
+				
+				long tiempo = System.nanoTime() - tiempoInicial;
+				
+				bf.write("El tiempo de envio del archivo fue de : "+ (tiempo)/(1000000)+ "segundos" + " \n");
+
 					 
 				bis.close();
-
-				System.out.println("No exploté");
+				
+				bf.close();
 			}
 		}
 		
 	}
+	
+	public static String resultadoDigest(byte[] paquete) throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		
+		md.update(paquete);
+		byte[] digest = md.digest();      
+
+		StringBuffer hexString = new StringBuffer();
+
+		for (int i = 0;i<digest.length;i++) 
+		{
+			hexString.append(Integer.toHexString(0xFF & digest[i]));
+		}
+		String prueba = hexString.toString();
+		
+		return prueba;
+	}
+	
+	
+	
 }
